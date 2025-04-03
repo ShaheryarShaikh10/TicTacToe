77ed0415ea46e3dc3680d39d63188385819ed832
ShaheryarShaikh10 <127825878+ShaheryarShaikh10@users.noreply.github.com>
2025-04-03 10:32:32 +0530

    handling state sequence execution

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,11 +2,14 @@ import { useState } from "react"
 export default function Player({name,symbol}){
     const [isEditing,setIsEditing]=useState(false);
     function handleEditClick(){
-        setIsEditing(true)
+        //we faced issue where we were unable to close the texbox by clicking on button 
+        // as state should be changed based on old state
+        setIsEditing((editing)=>!isEditing)
     }
     let playerName=<span className="player-name">{name}</span>
     if(isEditing){
-        playerName=<input type="text" required/>
+        //This line of code display current name in textbox
+        playerName=<input type="text" required value={name}/>
     }
     return(
         <li>
@@ -14,7 +17,7 @@ export default function Player({name,symbol}){
               {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing?"Save":"Edit"}</button>
           </li>
     )
 }

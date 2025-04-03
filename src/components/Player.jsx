5779eeaafe922703eedf83fd9631916bafb21681
ShaheryarShaikh10 <127825878+ShaheryarShaikh10@users.noreply.github.com>
2025-04-03 10:48:14 +0530

    two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react"
-export default function Player({name,symbol}){
+export default function Player({initialName,symbol}){
     const [isEditing,setIsEditing]=useState(false);
+    const [playerName,setPlayerName]=useState(initialName);
     function handleEditClick(){
         //we faced issue where we were unable to close the texbox by clicking on button 
         // as state should be changed based on old state
-        setIsEditing((editing)=>!isEditing)
+        setIsEditing((editing)=>!editing)
     }
-    let playerName=<span className="player-name">{name}</span>
+    function handleChange(event)
+    {
+        console.log(event)
+        setPlayerName(event.target.value)
+    }
+    let editablePlayerName=<span className="player-name">{playerName}</span>
+    //btnCaption="Edit"
     if(isEditing){
         //This line of code display current name in textbox
-        playerName=<input type="text" required value={name}/>
+        editablePlayerName=<input type="text" required value={playerName} onChange={handleChange}/>
+        //btnCaption="Save"
     }
     return(
         <li>
             <span className="player">
-              {playerName}
+              {editablePlayerName}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing?"Save":"Edit"}</button>
